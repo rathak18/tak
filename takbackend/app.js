55d93ed966b60e423b0f71d137ef7689a05d78eb
Rajendra Kumar Pathak <rajendrapathak1988@gmail.com>
2022-02-18 08:04:16 +0530

    added intial setup for tak backend

---
+++ takbackend/app.js
@@ -0,0 +1,13 @@
+const exp = require('constants');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const bodyParser = require(body-parser);
+
+app.use(bodyParser.json());
+
+app.listen(port, ()=>{
+    console.log("server started at",port);
+})
+
